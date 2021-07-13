67d814559c03b686cf10e6a331a437bfa2adf76c
Scott Coupar <scott.coupar@googlemail.com>
2021-07-13 22:57:47 +0100

    toggle between components now working

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,6 +10,7 @@ import { Link } from "react-router-dom";
 const Game = ({ name, released, image, id }) => {
   const dispatch = useDispatch();
   const loadDetailhandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
   return (
@@ -29,6 +30,7 @@ const StyledGame = styled(motion.div)`
   text-align: center;
   border-radius: 1rem;
   cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 40vh;
