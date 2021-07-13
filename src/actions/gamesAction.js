4f0c829eb785a3a9321ffc9bdd0294104de2a62e
Scott Coupar <scott.coupar@googlemail.com>
2021-07-13 20:08:49 +0100

    fetched upcoming and new games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
 
 //Action Creator
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const ucpomingData = await axios.get(upcomingGamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: ucpomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
