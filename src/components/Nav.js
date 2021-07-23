63dc787652b7edf4e85f6269d917e87339d4d25f
Scott Coupar <scott.coupar@googlemail.com>
2021-07-23 12:02:48 +0100

    added nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+//Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import logo from "../img/logo.svg";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <Logo>
+        <img src={logo} alt="logo" />
+        <h1>Ignite</h1>
+      </Logo>
+      <div className="search">
+        <input type="text" />
+        <button>Search</button>
+      </div>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled(motion.nav)``;
+const Logo = styled(motion.div)``;
+export default Nav;
